8f72ec620ba99e0e244caad38fe0edd67f7f4812
Carrie Lu <43943721+carrieluuu@users.noreply.github.com>
2022-06-24 13:51:45 -0700

    added floating action button to link onboarding screen to login/registration page

--- app/src/main/java/com/example/spork/onboarding/OnboardingFragment4.java
+++ app/src/main/java/com/example/spork/onboarding/OnboardingFragment4.java
@@ -1,6 +1,8 @@
 package com.example.spork.onboarding;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,13 +12,28 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.spork.R;
+import com.example.spork.login.LoginActivity;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class OnboardingFragment4 extends Fragment {
+
+    private FloatingActionButton fab;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         ViewGroup root = (ViewGroup) inflater.inflate(R.layout.fragment_onboarding4, container, false);
+
+        fab = root.findViewById(R.id.fab);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
 
         return root;
     }
