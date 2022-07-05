23d3fcee023e225b5105009a244f307b4e0075ae
Carrie Lu <43943721+carrieluuu@users.noreply.github.com>
2022-07-05 11:23:24 -0700

    display nearby restaurants on map

--- app/src/main/java/com/example/spork/home/HomeFragment.java
+++ app/src/main/java/com/example/spork/home/HomeFragment.java
@@ -1,26 +1,48 @@
 package com.example.spork.home;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.widget.SearchView;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import android.os.Looper;
+import android.provider.Settings;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
+import com.example.spork.BuildConfig;
 import com.example.spork.R;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.parse.ParseFile;
 import com.parse.ParseUser;
 
@@ -30,9 +52,16 @@ import com.parse.ParseUser;
 public class HomeFragment extends Fragment {
 
     private static final String TAG = "HomeFragment";
+    private static final int REQUEST_CODE = 100;
+    public static final int RADIUS =  5000;
 
+    private GoogleMap mMap;
+    private FusedLocationProviderClient client;
     private SearchView svMap;
     private ImageView ivProfile;
+    private double currentLat;
+    private double currentLng;
+    private String url;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -64,32 +93,190 @@ public class HomeFragment extends Fragment {
                     .into(ivProfile);
         }
 
+        client = LocationServices.getFusedLocationProviderClient(getContext());
+
         SupportMapFragment mapFragment =
                 (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
         if (mapFragment != null) {
             mapFragment.getMapAsync(callback);
         }
+
+        checkPermissions();
+
+        StringBuilder sb = new StringBuilder("https://maps.googleapis.com/maps/api/place/findplacefromtext/json");
+        sb.append("?fields=name%2Cgeometry/location");
+        sb.append("&input=restaurant");
+        sb.append("&inputtype=textquery");
+        sb.append("&locationbias=circle%3A" + RADIUS + currentLat + "%2C" + currentLng);
+        sb.append("&key=" + BuildConfig.MAPS_API_KEY);
+
+        url = sb.toString();
+        Log.i(TAG, url);
+
+
     }
 
     private OnMapReadyCallback callback = new OnMapReadyCallback() {
 
-        /**
-         * Manipulates the map once available.
-         * This callback is triggered when the map is ready to be used.
-         * This is where we can add markers or lines, add listeners or move the camera.
-         * In this case, we just add a marker near Sydney, Australia.
-         * If Google Play services is not installed on the device, the user will be prompted to
-         * install it inside the SupportMapFragment. This method will only be triggered once the
-         * user has installed Google Play services and returned to the app.
-         */
-
         @Override
         public void onMapReady(GoogleMap googleMap) {
-            LatLng sydney = new LatLng(-34, 151);
-            googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-            googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+            mMap = googleMap;
+
+            Object dataFetch[] = new Object[2];
+            dataFetch[0] = mMap;
+            dataFetch[1] = url;
+
+            FetchData fetchData  = new FetchData();
+            fetchData.execute(dataFetch);
+
+            // Add marker at current location
+            getCurrentLocation();
+
         }
     };
+
+
+    private void checkPermissions() {
+        if (ContextCompat.checkSelfPermission(
+                getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ContextCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(
+                    new String[] {
+                            Manifest.permission
+                                    .ACCESS_FINE_LOCATION,
+                            Manifest.permission
+                                    .ACCESS_COARSE_LOCATION },
+                    REQUEST_CODE);
+        }
+        else {
+            getCurrentLocation();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(
+            int requestCode, @NonNull String[] permissions,
+            @NonNull int[] grantResults)
+    {
+        super.onRequestPermissionsResult(
+                requestCode, permissions, grantResults);
+        // Check condition
+        if (requestCode == 100 && (grantResults.length > 0)
+                && (grantResults[0] + grantResults[1]
+                == PackageManager.PERMISSION_GRANTED)) {
+            // When permission are granted
+            // Call  method
+            getCurrentLocation();
+        }
+        else {
+            // When permission are denied
+            // Display toast
+            Toast
+                    .makeText(getActivity(),
+                            "Permission denied",
+                            Toast.LENGTH_SHORT)
+                    .show();
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private void getCurrentLocation()
+    {
+        // Initialize Location manager
+        LocationManager locationManager
+                = (LocationManager)getActivity()
+                .getSystemService(
+                        Context.LOCATION_SERVICE);
+        // Check condition
+        if (locationManager.isProviderEnabled(
+                LocationManager.GPS_PROVIDER)
+                || locationManager.isProviderEnabled(
+                LocationManager.NETWORK_PROVIDER)) {
+            // When location service is enabled
+            // Get last location
+            client.getLastLocation().addOnCompleteListener(
+                    new OnCompleteListener<Location>() {
+                        @Override
+                        public void onComplete(
+                                @NonNull Task<Location> task)
+                        {
+
+                            // Initialize location
+                            Location location
+                                    = task.getResult();
+                            // Check condition
+                            if (location != null) {
+                                // When location result is not
+                                // null set latitude
+                                currentLat = location.getLatitude();
+
+                                // set longitude
+                                currentLng = location.getLongitude();
+
+                            }
+                            else {
+                                // When location result is null
+                                // initialize location request
+                                LocationRequest locationRequest
+                                        = new LocationRequest()
+                                        .setPriority(
+                                                LocationRequest
+                                                        .PRIORITY_HIGH_ACCURACY)
+                                        .setInterval(10000)
+                                        .setFastestInterval(
+                                                1000)
+                                        .setNumUpdates(1);
+
+                                // Initialize location call back
+                                LocationCallback
+                                        locationCallback
+                                        = new LocationCallback() {
+                                    @Override
+                                    public void
+                                    onLocationResult(
+                                            LocationResult
+                                                    locationResult)
+                                    {
+                                        // Initialize
+                                        // location
+                                        Location location1
+                                                = locationResult
+                                                .getLastLocation();
+                                        // Set latitude
+                                        currentLat = location1.getLatitude();
+
+                                        // Set longitude
+                                        currentLng = location1.getLongitude();
+                                    }
+                                };
+
+                                // Request location updates
+                                client.requestLocationUpdates(
+                                        locationRequest,
+                                        locationCallback,
+                                        Looper.myLooper());
+                            }
+
+                            LatLng currentLatLng = new LatLng(currentLat, currentLng);
+                            mMap.addMarker(new MarkerOptions().position(currentLatLng).title("Current Location").icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_current_location)));
+                            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 10));
+
+                            Log.i(TAG, "current lat: " + currentLat +" and current lng: " + currentLng);
+
+                        }
+                    });
+        }
+        else {
+            // When location service is not enabled
+            // open location setting
+            startActivity(
+                    new Intent(
+                            Settings
+                                    .ACTION_LOCATION_SOURCE_SETTINGS)
+                            .setFlags(
+                                    Intent.FLAG_ACTIVITY_NEW_TASK));
+        }
+    }
 
 
 }
