09535b5f8bf46371a8f9bce12e6db8d3881ecf5c
Carrie Lu <43943721+carrieluuu@users.noreply.github.com>
2022-07-22 10:53:25 -0700

    create file utils class to generate places api url

---
+++ app/src/main/java/com/example/spork/FileUtils.java
@@ -0,0 +1,14 @@
+package com.example.spork;
+
+public class FileUtils {
+
+    public static String buildPlacesUrl(double currentLat, double currentLng, int radius) {
+        StringBuilder sb = new StringBuilder("https://maps.googleapis.com/maps/api/place/nearbysearch/json");
+        sb.append("?fields=name%2Cgeometry/location");
+        sb.append("&location=" + currentLat + "%2C" + currentLng);
+        sb.append("&radius=" + radius);
+        sb.append("&type=restaurant");
+        sb.append("&key=" + BuildConfig.MAPS_API_KEY);
+        return sb.toString();
+    }
+}
