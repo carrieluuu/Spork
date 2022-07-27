6ba4da0edf86b14ae03fb35a18daf7da2976c94e
Carrie Lu <43943721+carrieluuu@users.noreply.github.com>
2022-07-27 11:48:36 -0700

    remove menu toolbar

--- app/src/main/java/com/example/spork/MainActivity.java
+++ app/src/main/java/com/example/spork/MainActivity.java
@@ -5,24 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Menu;
 import android.view.MenuItem;
 
-import com.example.spork.feed.ComposeFragment;
 import com.example.spork.feed.FeedFragment;
 import com.example.spork.home.HomeFragment;
-import com.example.spork.login.LoginActivity;
 import com.example.spork.profile.ProfileFragment;
 import com.example.spork.search.SearchFragment;
 import com.google.android.libraries.places.api.Places;
 import com.google.android.libraries.places.api.net.PlacesClient;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.parse.LogOutCallback;
-import com.parse.ParseException;
-import com.parse.ParseUser;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -46,32 +38,6 @@ public class MainActivity extends AppCompatActivity {
         PlacesClient placesClient = Places.createClient(this);
 
 
-    }
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-
-        if (item.getItemId() == R.id.btnLogout) {
-            ParseUser.logOutInBackground(new LogOutCallback() {
-                @Override
-                public void done(ParseException e) {
-                    if (e != null) {
-                        Log.e(TAG, "Issue with logout", e);
-                        return;
-                    }
-                    goLoginActivity();
-                }
-            });
-
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     private void bottomNavigation() {
@@ -104,11 +70,6 @@ public class MainActivity extends AppCompatActivity {
         });
         // Set default selection
         bottomNavigationView.setSelectedItemId(R.id.action_home);
-    }
-
-    private void goLoginActivity() {
-        Intent i = new Intent(this, LoginActivity.class);
-        startActivity(i);
     }
 
 }
