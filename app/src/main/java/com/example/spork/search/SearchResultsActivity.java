2cb320700f940bdb88274124dceebc0de301df62
Carrie Lu <43943721+carrieluuu@users.noreply.github.com>
2022-07-28 15:16:33 -0700

    change status bar to transparent

--- app/src/main/java/com/example/spork/search/SearchResultsActivity.java
+++ app/src/main/java/com/example/spork/search/SearchResultsActivity.java
@@ -3,7 +3,11 @@ package com.example.spork.search;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
 import com.example.spork.R;
 import com.example.spork.Restaurant;
 import com.google.android.material.progressindicator.CircularProgressIndicator;
@@ -25,6 +29,11 @@ public class SearchResultsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_results);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            Window w = getWindow();
+            w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        }
 
         rvResults = findViewById(R.id.rvResults);
         loadingCircle = findViewById(R.id.loading_circle);
